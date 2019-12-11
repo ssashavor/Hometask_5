9cd95148cd04f5b4b414618629198019c86f52c0
ssashavor <47754110+ssashavor@users.noreply.github.com>
2019-12-11 23:59:07 +0300

    Update sscanf_conv.go

--- sscanf_conv.go
+++ sscanf_conv.go
@@ -6,7 +6,7 @@ import "fmt"
 func convSScanf(s string) (int, error) {
 	var i int
 	if _, err := fmt.Sscanf(s, "%d ", &i); err != nil {
-		return 0, fmt.Errorf("Not correct input.")
+		return 0, err
 	}
 	return i, nil
 }
